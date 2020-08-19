419b29f154e84b63c00e8175807e9442b6ea2168
jami-raza <57630996+jami-raza@users.noreply.github.com>
2020-08-19 09:24:42 +0500

    update

---
+++ project4-ui-clone/src/Components/Card2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Icon from './Svg/Banner'
+
+export default function SecondCard() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+          <Icon />
+    </div>
+  );
+}
